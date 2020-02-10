6ead2bdbfa3d7b26cb6756e4a8e7ddcd459018ae
Thorvald van Beek <tvanbeek@roc-dev.com>
2020-02-10 13:41:28 +0100

    File renamed

--- game.php
+++ game.php
@@ -1,28 +1,31 @@
 <?php
 session_start();
-
 ?>
 
 <!DOCTYPE html>
-<link href="style.css" rel="stylesheet">
-<link href="rps.png" rel="icon" type="icon.png" />
 <html lang="nl-NL">
-<meta charset="UTF-8">
-<title>Steen, Papier, Schaar</title>
 
 <head>
-    <H1>Steen, Papier, Schaar</H1>
+    <link href="style.css" rel="stylesheet">
+    <link href="rps.png" rel="icon" type="icon.png" />
+    <meta charset="UTF-8">
+    <title>Steen, Papier, Schaar</title>
+</head>
 
 <body>
+    <H1>Steen, Papier, Schaar</H1>
+
+
     <h3>Speler 1</h3>
     <?php
+
     if (isset($_POST['RPS'])) {
-        $p1 = false;
+        $input = false;
     } else {
-        $p1 = true;
+        $input = true;
     }
-    if ($p1 === true) {
-        echo ("<form id='p1' action='php.php' method='POST'>
+    if ($input === true) {
+        echo ("<form id='input' method='POST'>
         <input type='radio' name='RPS' value='Steen'>Steen<br>
         <input type='radio' name='RPS' value='Papier'>Papier<br>
         <input type='radio' name='RPS' value='Schaar'>Schaar<br>
@@ -30,8 +33,9 @@ session_start();
     </form>");
 
         if (isset($_POST['RPS'])) {
+            $_SESSION['player1'] = $_POST['RPS'];
             setcookie("player1", $_POST['RPS']);
-            $p1 = false;
+            $input = false;
         }
     } else
         echo "";
@@ -39,8 +43,8 @@ session_start();
     <h3>Speler 2</h3>
     <?php
     $p2 = false;
-    if ($p1 === false) {
-        echo ("<form id='p2' action='php.php' method='POST'>
+    if ($input === false) {
+        echo ("<form id='p2' action='spel.php' method='POST'>
         <input type='radio' name='RPS2' value='Steen'>Steen<br>
         <input type='radio' name='RPS2' value='Papier'>Papier<br>
         <input type='radio' name='RPS2' value='Schaar'>Schaar<br>
@@ -48,13 +52,15 @@ session_start();
     </form>");
 
         if (isset($_POST['RPS2'])) {
+            $_SESSION['player2'] = $_POST['RPS2'];
             setcookie("player2", $_POST['RPS2']);
             $p2 = false;
         }
-    } else
+    } else {
         echo "";
+    }
     ?>
 </body>
-</head>
+
 
 </html>
