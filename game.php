005c54b4be3d33c026598cd14df01aae0c9fadd3
Thorvald van Beek <tvanbeek@roc-dev.com>
2020-02-10 13:45:23 +0100

    Uitwerking van de exercise

--- game.php
+++ game.php
@@ -1,66 +1,102 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="nl-NL">
-
+<html class="backgroundcolor">
+<?php session_start(); ?>
 <head>
-    <link href="style.css" rel="stylesheet">
-    <link href="rps.png" rel="icon" type="icon.png" />
-    <meta charset="UTF-8">
-    <title>Steen, Papier, Schaar</title>
+    <title>
+        steen_papier_schaar
+    </title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
-    <H1>Steen, Papier, Schaar</H1>
+<body class="color">
+    <h1>
+        Steen_Papier_Schaar!
+    </h1>
 
-
-    <h3>Speler 1</h3>
     <?php
-
-    if (isset($_POST['RPS'])) {
-        $input = false;
-    } else {
-        $input = true;
-    }
-    if ($input === true) {
-        echo ("<form id='input' method='POST'>
-        <input type='radio' name='RPS' value='Steen'>Steen<br>
-        <input type='radio' name='RPS' value='Papier'>Papier<br>
-        <input type='radio' name='RPS' value='Schaar'>Schaar<br>
-        <input type='submit' value='Submit'>
-    </form>");
-
-        if (isset($_POST['RPS'])) {
-            $_SESSION['player1'] = $_POST['RPS'];
-            setcookie("player1", $_POST['RPS']);
-            $input = false;
-        }
-    } else
-        echo "";
+            if(isset($_POST['steen'])){
+                $_SESSION['player:1'] = 'steen';
+            }
+            if(isset($_POST['papier'])){
+                $_SESSION['player:1'] = 'papier';
+            }
+            if(isset($_POST['schaar'])){
+                $_SESSION['player:1'] = 'schaar';
+            }
+            if(isset($_POST['steen2'])){
+                $_SESSION['player:2'] = 'steen';
+            }
+            if(isset($_POST['papier2'])){
+                $_SESSION['player:2'] = 'papier';
+            }
+            if(isset($_POST['schaar2'])){
+                $_SESSION['player:2'] = 'schaar';
+            }
     ?>
-    <h3>Speler 2</h3>
-    <?php
-    $p2 = false;
-    if ($input === false) {
-        echo ("<form id='p2' action='spel.php' method='POST'>
-        <input type='radio' name='RPS2' value='Steen'>Steen<br>
-        <input type='radio' name='RPS2' value='Papier'>Papier<br>
-        <input type='radio' name='RPS2' value='Schaar'>Schaar<br>
-        <input type='submit' value='Submit'>
-    </form>");
 
-        if (isset($_POST['RPS2'])) {
-            $_SESSION['player2'] = $_POST['RPS2'];
-            setcookie("player2", $_POST['RPS2']);
-            $p2 = false;
+    <div>
+            <?php 
+                if($_SESSION['player:1'] == $_SESSION['player:2'] && isset($_POST['winnaarbekijken'])){
+                    echo "Gelijkspel";
+                }
+                if($_SESSION['player:1'] == 'steen' && $_SESSION['player:2'] == 'papier' && isset($_POST['Gewonnen'] )){
+                    echo "Player:2 Heeft_Gewonnen";
+                }
+                if($_SESSION['player:1'] == 'papier' && $_SESSION['player:2'] == 'schaar' && isset($_POST['Gewonnen'] )){
+                    echo "Player:2 Heeft_Gewonnen";
+                }
+                if($_SESSION['player:1'] == 'schaar' && $_SESSION['player:2'] == 'steen' && isset($_POST['Gewonnen'] )){
+                    echo "Player:2 Heeft_Gewonnen";
+                }
+                if($_SESSION['player:2'] == 'steen' && $_SESSION['player:1'] == 'papier' && isset($_POST['Gewonnen'] )){
+                    echo "Player:1 Heeft_Gewonnen";
+                }
+                if($_SESSION['player:2'] == 'papier' && $_SESSION['player:1'] == 'schaar' && isset($_POST['Gewonnen'] )){
+                    echo "Player:1 Heeft_Gewonnen";
+                }
+                if($_SESSION['player:2'] == 'schaar' && $_SESSION['player:1'] == 'steen' && isset($_POST['Gewonnen'] )){
+                    echo "Player:1 Heeft_Gewonnen";
+                }
+                
+
+            ?>
+    </div>
+
+    <div>
+        <?php if(isset($_POST['winnaarbekijken'])) {
+            echo "Speler 1:". $_SESSION['player:1'];
+        }else{
+            echo "Speler 1:";
         }
-    } else {
-        echo "";
-    }
-    ?>
+        ?>
+        <br>
+        <br>
+        <?php if(isset($_POST['winnaarbekijken'])) {
+        echo "Speler 2:". $_SESSION['player:2'];
+        }else{
+            echo "Speler 2:";
+        }
+        ?>
+    </div>
+
+<form  method="POST">
+    <div class="playereen">
+        <h3>Speler_1</h3>
+            <input name="steen" type="submit" value="steen">
+            <input name="papier" type="submit" value="papier">
+            <input name="schaar" type="submit" value="schaar">
+    </div>
+    <div class="playertwee">
+        <h3>Speler_2</h3>
+            <input name="steen2" type="submit" value="steen">
+            <input name="papier2" type="submit" value="papier">
+            <input name="schaar2" type="submit" value="schaar">
+    </div>
+    <div class="showbutton">
+            <h3>Winnaar_Weergeven</h3>
+            <input name="winnaarbekijken" type="submit" value="Winnaar_Bekijken">
+    </div>
+</form>
 </body>
-
 
 </html>
